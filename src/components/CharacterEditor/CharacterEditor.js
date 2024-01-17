1860bbaca2311dffd8dd0a9e62850924db25721e
Matthew Tucker-Simmons <tucker.simmons@gmail.com>
2024-01-17 15:09:38 -0500

    add fixed perspective pane

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -38,6 +38,9 @@ function App() {
             controls below. What sort of adventure will you embark on?{' '}
           </p>
         </header>
+
+        <div className={styles.perspectivePane} />
+
         <div className={styles.controlColumn}>
           <ControlPane
             title="Bodies"
